bc7804e14e267151ab5f9af807615ccfee6573cd
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2023-03-11 13:28:55 +0100

    run command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Build system profiler application");
+    // run application specified in process arguments
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("No application specified");
+        return;
+    }
+
+    let app = &args[1];
+    let mut cmd = std::process::Command::new(app)
+        .args(&args[2..])
+        .spawn()
+        .unwrap();
+
+    cmd.wait().unwrap();
 }
