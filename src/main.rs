b0b08988dbfa22fdb898ff4a3616faadb1d626c4
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2023-03-11 14:44:00 +0100

    split info from main

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,10 @@
 use serde::Serialize;
-use sysinfo::{CpuExt, SystemExt};
 
-#[derive(Serialize)]
-struct SystemInfo {
-    num_cpus: u32,
-    cpu_name: String,
-    total_memory: u64,
-    os: String,
-}
-
-fn get_system_info() -> SystemInfo {
-    let sys = sysinfo::System::new_all();
-    let cpu = &sys.cpus()[0];
-
-    let os_name = get_system_name(&sys);
-
-    SystemInfo {
-        num_cpus: sys.cpus().len() as u32,
-        cpu_name: cpu.vendor_id().to_string() + " / " + cpu.brand(),
-        total_memory: sys.total_memory(),
-        os: os_name,
-    }
-}
-
-fn get_system_name(sys: &sysinfo::System) -> String {
-    let mut os_name;
-    match sys.name() {
-        Some(name) => {
-            os_name = name;
-            match sys.os_version() {
-                Some(version) => {
-                    os_name += " ";
-                    os_name += &version;
-                }
-                None => {}
-            }
-            match sys.kernel_version() {
-                Some(version) => {
-                    os_name += " (";
-                    os_name += &version;
-                    os_name += ")";
-                }
-                None => {}
-            }
-        }
-        None => {
-            os_name = "Unknown".to_string();
-        }
-    }
-    os_name
-}
-
-impl SystemInfo {
-    fn print(&self) {
-        println!("OS name       \t: {}", self.os);
-        println!("CPU name      \t: {}", self.cpu_name);
-        println!("Number of CPUs\t: {}", self.num_cpus);
-        println!("Total memory  \t: {} MB", self.total_memory / 1024 / 1024);
-    }
-}
+mod info;
 
 #[derive(Serialize)]
 struct ProfileReport {
-    system_info: SystemInfo,
+    system_info: info::SystemInfo,
     cmd_name: String,
     cmd_args: Vec<String>,
 }
@@ -76,7 +18,7 @@ fn main() {
         return;
     }
 
-    let sys_info = get_system_info();
+    let sys_info = info::get_system_info();
     sys_info.print();
 
     let cmd_name = &args[1];
@@ -102,6 +44,6 @@ fn main() {
         cmd_args: cmd_args.to_vec(),
     };
 
-    let report_json = serde_json::to_string(&report).unwrap();
+    let report_json = serde_json::to_string_pretty(&report).unwrap();
     println!("{}", report_json);
 }
