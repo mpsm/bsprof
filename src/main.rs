69a05f84cdb87c821aa5c9742ee8f9adfc3e8405
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2023-03-11 13:44:09 +0100

    printing system info at startup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use sysinfo::{CpuExt, SystemExt};
+
+struct SystemInfo {
+    num_cpus: u32,
+    cpu_name: String,
+    total_memory: u64,
+}
+
+fn get_system_info() -> SystemInfo {
+    let sys = sysinfo::System::new_all();
+    let cpu = &sys.cpus()[0];
+    SystemInfo {
+        num_cpus: sys.cpus().len() as u32,
+        cpu_name: cpu.vendor_id().to_string() + " / " + cpu.brand(),
+        total_memory: sys.total_memory(),
+    }
+}
+
+impl SystemInfo {
+    fn print(&self) {
+        println!("CPU name      \t: {}", self.cpu_name);
+        println!("Number of CPUs\t: {}", self.num_cpus);
+        println!("Total memory  \t: {} MB", self.total_memory / 1024 / 1024);
+    }
+}
+
 fn main() {
     println!("Build system profiler application");
     // run application specified in process arguments
@@ -7,9 +33,19 @@ fn main() {
         return;
     }
 
-    let app = &args[1];
-    let mut cmd = std::process::Command::new(app)
-        .args(&args[2..])
+    let sys_info = get_system_info();
+    sys_info.print();
+
+    let cmd_name = &args[1];
+    let cmd_args = &args[2..];
+    if cmd_args.len() > 0 {
+        println!("Running command: {} {:?}", cmd_name, cmd_args);
+    } else {
+        println!("Running command: {}", cmd_name);
+    }
+
+    let mut cmd = std::process::Command::new(cmd_name)
+        .args(cmd_args)
         .spawn()
         .unwrap();
 
