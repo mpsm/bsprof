100afcbbdbd42b12d303d31d4746a7b25815af28
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2023-03-12 14:46:54 +0100

    Add argument parsing

--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,51 @@ use std::fs::File;
 mod profile;
 
 struct Args {
-    interval_ms: u32,
+    interval: u32,
+    warmup: u32,
     command: String,
     args: Vec<String>,
 }
 
-fn parse_args(_cmd: &clap::Command) -> Args {
-    Args {
-        interval_ms: 0,
-        command: String::new(),
-        args: Vec::<String>::new(),
-    }
-}
-
 fn main() {
-    let mut cmd = clap::Command::new("Build System Profiler")
+    let cmd = clap::Command::new("Build System Profiler")
         .author("Marcin Smoczyński, smoczynski.marcin@gmail.com")
         .about("A simple tool for profiling build systems")
         .arg(
             clap::Arg::new("interval_ms")
+                .help("Interval in ms between data points")
                 .short('i')
                 .long("interval")
                 .default_value("1000"),
         )
-        .trailing_var_arg(true);
+        .arg(
+            clap::Arg::new("warmup_ms")
+                .short('w')
+                .long("warmup")
+                .help("Warmup time in ms"),
+        )
+        .arg(
+            clap::Arg::new("command")
+                .required(true)
+                .help("Command to run"),
+        )
+        .arg(
+            clap::Arg::new("args")
+                .num_args(0..)
+                .help("Command arguments"),
+        );
 
-    cmd.print_long_help().unwrap();
-
-    let args = parse_args(&cmd);
+    let args = parse_args(cmd).unwrap();
 
     let sys_info = profile::info::get_system_info();
     sys_info.print();
 
-    if args.args.len() > 0 {
-        println!("Running command: {} {:?}", args.command, args.args);
-    } else {
-        println!("Running command: {}", args.command);
-    }
+    println!("Profiling command: {}", args.command);
+    println!("Profiling args: {:?}", args.args);
+    println!("Profiling warmup: {} ms", args.warmup);
+    println!("Profiling interval: {} ms", args.interval);
 
-    let interval = std::time::Duration::from_millis(args.interval_ms as u64);
+    let interval = std::time::Duration::from_millis(args.interval as u64);
     let report = profile::profile(&args.command, &args.args, interval);
 
     println!("Done, saving report");
@@ -53,23 +59,31 @@ fn save_report(report: &profile::ProfileReport) {
     serde_json::to_writer_pretty(file, &report).unwrap();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_command() {
-        let args = Args::parse_from(&["bsprof", "ls"]);
-        assert_eq!(args.interval_ms, 1000);
-        assert_eq!(args.command, "ls");
-        assert_eq!(args.args, Vec::<String>::new());
-    }
-
-    #[test]
-    fn test_command_argumets() {
-        let args = Args::parse_from(&["bsprof", "ls", "-l", "-a"]);
-        assert_eq!(args.interval_ms, 1000);
-        assert_eq!(args.command, "ls");
-        assert_eq!(args.args, vec!["-l".to_string(), "-a".to_string()]);
-    }
+fn parse_args(cmd: clap::Command) -> Result<Args, &'static str> {
+    let m = cmd.get_matches();
+    let interval = m
+        .get_one::<String>("interval_ms")
+        .unwrap()
+        .parse::<u32>()
+        .unwrap();
+    let command = match m.get_one::<String>("command") {
+        Some(cmd) => cmd.to_owned(),
+        None => {
+            return Err("No command specified");
+        }
+    };
+    let warmup = match m.get_one::<String>("warmup_ms") {
+        Some(w) => w.parse::<u32>().unwrap(),
+        None => 0,
+    };
+    let cmdargs: Vec<String> = match m.get_many::<String>("args") {
+        Some(args) => args.map(|x| x.to_owned()).collect(),
+        None => Vec::new(),
+    };
+    Ok(Args {
+        interval: interval,
+        warmup: warmup,
+        command: command,
+        args: cmdargs,
+    })
 }
