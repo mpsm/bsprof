fba107d8f56a600bad07e78d3cd80d58d2d022e6
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2023-03-12 13:35:57 +0100

    clap wip

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
 use std::fs::File;
 
-use clap::Parser;
-
 mod profile;
 
-#[derive(Parser, Debug)]
-#[command(name = "bsprof")]
-#[command(author = "Marcin Smoczyński <smoczynski.marcin@gmail.com>")]
-#[command(version)]
-#[command(about = "A simple tool for profiling build systems", long_about = None)]
 struct Args {
-    #[arg(short, long, default_value_t = 1000)]
     interval_ms: u32,
     command: String,
     args: Vec<String>,
 }
 
+fn parse_args(_cmd: &clap::Command) -> Args {
+    Args {
+        interval_ms: 0,
+        command: String::new(),
+        args: Vec::<String>::new(),
+    }
+}
+
 fn main() {
-    let args = Args::parse();
+    let mut cmd = clap::Command::new("Build System Profiler")
+        .author("Marcin Smoczyński, smoczynski.marcin@gmail.com")
+        .about("A simple tool for profiling build systems")
+        .arg(
+            clap::Arg::new("interval_ms")
+                .short('i')
+                .long("interval")
+                .default_value("1000"),
+        )
+        .trailing_var_arg(true);
+
+    cmd.print_long_help().unwrap();
+
+    let args = parse_args(&cmd);
 
     let sys_info = profile::info::get_system_info();
     sys_info.print();
@@ -38,4 +51,25 @@ fn main() {
 fn save_report(report: &profile::ProfileReport) {
     let file = File::create("report.json").unwrap();
     serde_json::to_writer_pretty(file, &report).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_command() {
+        let args = Args::parse_from(&["bsprof", "ls"]);
+        assert_eq!(args.interval_ms, 1000);
+        assert_eq!(args.command, "ls");
+        assert_eq!(args.args, Vec::<String>::new());
+    }
+
+    #[test]
+    fn test_command_argumets() {
+        let args = Args::parse_from(&["bsprof", "ls", "-l", "-a"]);
+        assert_eq!(args.interval_ms, 1000);
+        assert_eq!(args.command, "ls");
+        assert_eq!(args.args, vec!["-l".to_string(), "-a".to_string()]);
+    }
 }
