f0e1f12866f3970a0f93b8e57e1040ea17bc2cc7
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2023-03-14 13:54:59 +0100

    generate sequential report

--- tools/genreport/genreport.py
+++ tools/genreport/genreport.py
@@ -38,6 +38,51 @@ def gen_cores_plot(times, cpu_cores_data, chart_id):
     plt.close()
 
 
+def gen_time_plot(jobs, elapsed, user, system):
+    plt.figure(figsize=(PLOT_WIDTH / PLOT_DPI, PLOT_HEIGHT / PLOT_DPI), dpi=PLOT_DPI)
+    plt.plot(jobs, elapsed, label="Elapsed Time", color="red")
+    plt.plot(jobs, user, label="User Time", color="blue")
+    plt.plot(jobs, system, label="System Time", color="green")
+    plt.plot(jobs, user + system, label="User + System Time", color="orange")
+    plt.xlabel("Jobs")
+    plt.ylabel("Time (s)")
+    plt.legend()
+    plt.title("Time")
+    plt.grid(True)
+    plt.savefig("time.png", dpi=PLOT_DPI)
+    plt.close()
+
+
+def gen_build_time_plot(jobs, elapsed):
+    plt.figure(figsize=(PLOT_WIDTH / PLOT_DPI, PLOT_HEIGHT / PLOT_DPI), dpi=PLOT_DPI)
+    plt.plot(jobs, elapsed, label="Elapsed Time", color="red")
+    plt.xlabel("Jobs")
+    plt.ylabel("Time (s)")
+    plt.legend()
+    plt.title("Build Time vs jobs")
+    plt.grid(True)
+    plt.savefig("build_time.png", dpi=PLOT_DPI)
+    plt.close()
+
+
+def gen_cpu_time_plot(jobs, elapsed_time, user_time, system_time, cores):
+    cpu_time = (user_time + system_time) / elapsed_time
+    plt.figure(figsize=(PLOT_WIDTH / PLOT_DPI, PLOT_HEIGHT / PLOT_DPI), dpi=PLOT_DPI)
+    plt.plot(jobs, jobs, label="Reference CPU utilization", color="red", linestyle="--")
+    plt.plot(jobs, cpu_time, label="Effective CPU utilization", color="blue")
+    plt.xlabel("Jobs")
+    plt.ylabel("Cores used")
+    plt.legend()
+    plt.title("Effective CPU utilization ({} cores)".format(cores))
+    plt.grid(True)
+    plt.savefig("cpu_time.png", dpi=PLOT_DPI)
+    plt.close()
+
+
+def rusage_json_time_to_float(rusage_time):
+    return float(rusage_time["secs"]) + float(rusage_time["nanos"]) / 1_000_000_000.0
+
+
 def main(args):
     if len(args) < 2:
         print("Usage: python genreport.py <datafile> [cores]")
@@ -89,6 +134,27 @@ def main(args):
 
         gen_usage_plot(times, cpu_usage, memory_percent_usage, jobs)
         gen_cores_plot(times, cpu_cores_data, jobs)
+
+    jobs = np.array([d["jobs"] for d in report["profile_results"]])
+    elapsed_time = np.array([d["elapsed_time"] for d in report["profile_results"]])
+    user_time = np.array(
+        [
+            rusage_json_time_to_float(d["rusage"]["user_time"])
+            for d in report["profile_results"]
+        ]
+    )
+    system_time = np.array(
+        [
+            rusage_json_time_to_float(d["rusage"]["system_time"])
+            for d in report["profile_results"]
+        ]
+    )
+
+    cores = report["system_info"]["num_cpus"]
+
+    gen_time_plot(jobs, elapsed_time, user_time, system_time)
+    gen_build_time_plot(jobs, elapsed_time)
+    gen_cpu_time_plot(jobs, elapsed_time, user_time, system_time, cores)
 
     return 0
 
