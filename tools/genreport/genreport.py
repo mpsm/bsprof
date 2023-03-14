6a6f87f1ee8c6fbdd336e9204fe0f167e9f2e9f5
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2023-03-14 09:31:31 +0100

    generate multiple reports

--- tools/genreport/genreport.py
+++ tools/genreport/genreport.py
@@ -9,34 +9,43 @@ PLOT_HEIGHT = 600
 PLOT_DPI = 72
 
 
-def gen_usage_plot(times, cpu_usage, memory_percent_usage):
+def gen_usage_plot(times, cpu_usage, memory_percent_usage, chart_id):
     plt.figure(figsize=(PLOT_WIDTH / PLOT_DPI, PLOT_HEIGHT / PLOT_DPI), dpi=PLOT_DPI)
     plt.plot(times, cpu_usage, label="CPU Usage", color="red")
     plt.plot(times, memory_percent_usage, label="Memory Usage", color="blue")
     plt.xlabel("Time (s)")
     plt.ylabel("Usage (%)")
     plt.legend()
-    plt.title("CPU and Memory Usage")
+    plt.title("CPU and Memory Usage, {} jobs".format(chart_id))
     plt.grid(True)
-    plt.savefig("cpu_memory_usage.png", dpi=PLOT_DPI)
+    plt.savefig("cpu_memory_usage_{:02d}.png".format(chart_id), dpi=PLOT_DPI)
     plt.close()
 
 
-def gen_cores_plot(times, cpu_cores_data):
+def gen_cores_plot(times, cpu_cores_data, chart_id):
     plt.figure(figsize=(PLOT_WIDTH / PLOT_DPI, PLOT_HEIGHT / PLOT_DPI), dpi=PLOT_DPI)
     for i, cpu_core_data in enumerate(cpu_cores_data):
         plt.plot(times, cpu_core_data, label="CPU Core {}".format(i))
     plt.grid(True)
     plt.xlabel("Time (s)")
     plt.ylabel("CPU Cores Utilization (%)")
-    plt.title("CPU Cores Utilization ({} cores)".format(len(cpu_cores_data)))
-    plt.savefig("cpu_cores_utilization.png")
+    plt.title(
+        "CPU Cores Utilization ({} cores, {} jobs)".format(
+            len(cpu_cores_data), chart_id
+        )
+    )
+    plt.savefig("cpu_cores_utilization_{:02d}.png".format(chart_id), dpi=PLOT_DPI)
     plt.close()
 
 
 def main(args):
     if len(args) < 2:
-        print("Usage: python genreport.py <datafile>")
+        print("Usage: python genreport.py <datafile> [cores]")
+        print("")
+        print("\t<datafile> - path to the data file")
+        print(
+            "\t[charts] - comma separated list of cores to generate charts (default: max cores used)"
+        )
         return 1
 
     filename = args[1]
@@ -46,26 +55,40 @@ def main(args):
         print("Could not open file: %s" % filename)
         return 1
 
-    report = json.load(fd)
-    profile_data = report["profile_results"][0]
-    times = np.array([d["elapsed"] for d in profile_data["datapoints"]])
-    cpu_usage = np.array([d["cpu_usage"] for d in profile_data["datapoints"]])
-    memory_usage = np.array([d["memory_usage"] for d in profile_data["datapoints"]])
-    cpus_utilization = np.array(
-        [d["cpus_utilization"] for d in profile_data["datapoints"]]
-    )
-    cpu_cores_data = list(map(np.array, zip(*cpus_utilization)))
-    memory_percent_usage = memory_usage / report["system_info"]["total_memory"] * 100
+    charts = [int(a) for a in args[2].split(",")] if len(args) > 2 else None
 
-    assert len(times) == len(cpu_usage) == len(memory_usage)
+    report = json.load(fd)
 
     plots_dir = "plots"
     if not os.path.exists(plots_dir):
         os.mkdir(plots_dir)
     os.chdir(plots_dir)
 
-    gen_usage_plot(times, cpu_usage, memory_percent_usage)
-    gen_cores_plot(times, cpu_cores_data)
+    # use the last profile data if charts is not specified
+    if charts is None:
+        charts = [report["profile_results"][-1]["jobs"]]
+
+    for profile_data in report["profile_results"]:
+        jobs = profile_data["jobs"]
+
+        if jobs not in charts:
+            continue
+
+        times = np.array([d["elapsed"] for d in profile_data["datapoints"]])
+        cpu_usage = np.array([d["cpu_usage"] for d in profile_data["datapoints"]])
+        memory_usage = np.array([d["memory_usage"] for d in profile_data["datapoints"]])
+        cpus_utilization = np.array(
+            [d["cpus_utilization"] for d in profile_data["datapoints"]]
+        )
+        cpu_cores_data = list(map(np.array, zip(*cpus_utilization)))
+        memory_percent_usage = (
+            memory_usage / report["system_info"]["total_memory"] * 100
+        )
+
+        assert len(times) == len(cpu_usage) == len(memory_usage)
+
+        gen_usage_plot(times, cpu_usage, memory_percent_usage, jobs)
+        gen_cores_plot(times, cpu_cores_data, jobs)
 
     return 0
 
