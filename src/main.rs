af49dadc65537f7a8a4abb97211254f5d3f85b00
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2023-03-14 14:01:45 +0100

    do not include clean in rusage

--- src/main.rs
+++ src/main.rs
@@ -20,17 +20,15 @@ fn main() {
     let mut build_cmd = cmd::Command::new(&args.command, &args.args);
     build_cmd.add_arg(&args.target);
 
-    let mut last_rusage = profile::rusage::get_process_rusage();
     for j in sequence {
         println!("Cleaning up");
         clean_command.run().unwrap();
 
         println!("Profiling with {} jobs", j);
 
+        let last_rusage = profile::rusage::get_process_rusage();
         let mut result = profile::profile(&build_cmd, &settings, j);
-        let corrected_rusage = result.rusage - last_rusage;
-        last_rusage = result.rusage;
-        result.rusage = corrected_rusage;
+        result.rusage = result.rusage - last_rusage;
         report.add_result(result);
     }
 
